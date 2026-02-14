d3ab74de1549f56bff8788554960cdd75352c2d2
qingxian997 <x1633787624@163.com>
2026-02-14 18:37:11 +0800

    完善首页逻辑并接入外部资源内容

---
+++ electron/preload.js
@@ -0,0 +1,12 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  getGames: () => ipcRenderer.invoke('games:get'),
+  addGame: (game) => ipcRenderer.invoke('games:add', game),
+  updateGame: (id, patch) => ipcRenderer.invoke('games:update', id, patch),
+  removeGame: (id) => ipcRenderer.invoke('games:remove', id),
+  launchGame: (id) => ipcRenderer.invoke('games:launch', id),
+  pickExecutable: () => ipcRenderer.invoke('games:pickExecutable'),
+  pickCoverFile: () => ipcRenderer.invoke('games:pickCoverFile'),
+  fetchCover: (title) => ipcRenderer.invoke('games:fetchCover', title),
+});
