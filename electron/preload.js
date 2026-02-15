7e82938ef8da952ffc59c1845a6cbee038cd0fa0
qingxian997 <x1633787624@163.com>
2026-02-15 15:10:24 +0800

    feat: 自动识别Steam AppID并优化封面与新闻来源

--- electron/preload.js
+++ electron/preload.js
@@ -10,6 +10,9 @@ contextBridge.exposeInMainWorld('electronAPI', {
   pickDirectory: () => ipcRenderer.invoke('games:pickDirectory'),
   pickCoverFile: () => ipcRenderer.invoke('games:pickCoverFile'),
   fetchCover: (game) => ipcRenderer.invoke('games:fetchCover', game),
+  fetchPortraitCover: (game) => ipcRenderer.invoke('games:fetchPortraitCover', game),
+  fetchLandscapeCover: (game) => ipcRenderer.invoke('games:fetchLandscapeCover', game),
+  resolveSteamAppId: (game) => ipcRenderer.invoke('games:resolveSteamAppId', game),
   getSteamGameInfo: (game, apiKey) => ipcRenderer.invoke('steam:getGameInfo', game, apiKey),
   getSteamCommunityFeed: (games, apiKey) => ipcRenderer.invoke('steam:getCommunityFeed', games, apiKey),
 });
